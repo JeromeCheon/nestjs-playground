ea2d8e9aed6f9d8396ffbe5da657d892ffbc4560
Jerome <cheonjh4572@gmail.com>
2022-05-26 14:28:21 +0900

    Update controller and service before thinking of DI

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { MessagesRepository } from './messages.repository';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+
+  constructor() {
+    // Servic is creating its own dependencies
+    // DONT DO THIS ON REAL APPS
+    this.messagesRepo = new MessagesRepository();
+  }
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
