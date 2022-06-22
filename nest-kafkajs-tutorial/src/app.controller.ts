e07c5a7bebcc6ea54d2a014e5545b5627e3a223c
Jerome <cheonjh4572@gmail.com>
2022-06-22 18:11:26 +0900

    Update to check the possibility by using url param for dynamic topics

--- nest-kafkajs-tutorial/src/app.controller.ts
+++ nest-kafkajs-tutorial/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello() {
-    return this.appService.getHello();
+  @Get(':id')
+  getHello(@Param('id', new ParseIntPipe()) id: number) {
+    return this.appService.getHello(id);
   }
 }
